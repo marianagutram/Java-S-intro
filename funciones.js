908db0dbce8a7b8a2e2d8d2bdeb4abce6526ed8a
mariana <gutierrezariana364@gmail.com>
2025-05-14 13:37:49 -0600

    funciones_ejercicio_libros

---
+++ funciones.js
@@ -0,0 +1,26 @@
+let numLibros = []
+
+
+function agregarLibro () {
+
+        let notaUser=window.prompt("Libros que hayas leído")
+    if (notaUser !=numLibros){
+        numLibros.push(notaUser);
+    }
+
+    }
+
+    agregarLibro()
+    agregarLibro()
+    agregarLibro()
+    agregarLibro()
+    
+    
+    
+    
+    
+    function librosLeidos(){
+    
+    }
+    console.log(numLibros)
+    librosLeidos()
